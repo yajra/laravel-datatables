3f3d4fd570920cf90f973708dcc32fac6918f8df
Arjay Angeles <aqangeles@gmail.com>
2015-03-27 09:08:34 +0800

    added v1.10 tests

--- tests/DatatablesBuilder.php
+++ tests/DatatablesBuilder.php
@@ -62,11 +62,61 @@ class DatatablesBuilderTest extends PHPUnit_Framework_TestCase  {
 		Config::shouldReceive('get');
 
 		// set Input variables
+		$_GET = [];
 		$_GET['sEcho'] = 1;
 
 		$response = Datatables::of($builder)->make();
 		$actual = $response->getContent();
 		$expected = '{"sEcho":1,"iTotalRecords":2,"iTotalDisplayRecords":2,"aaData":[[1,"foo"],[2,"bar"]],"sColumns":["id","name"]}';
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function test_datatables_make_with_data_version_1_10()
+	{
+		$cache = m::mock('stdClass');
+		$driver = m::mock('stdClass');
+		$data = array(
+			array('id' => 1, 'name' => 'foo'),
+			array('id' => 2, 'name' => 'bar'),
+			);
+		$builder = m::mock('Illuminate\Database\Query\Builder');
+		$builder->shouldReceive('select')->once()->with(array('id','name'))->andReturn($builder);
+		$builder->shouldReceive('from')->once()->with('users')->andReturn($builder);
+		$builder->shouldReceive('get')->once()->andReturn($data);
+
+		$builder->columns = array('id', 'name');
+		$builder->select(array('id', 'name'))->from('users');
+
+		// ******************************
+		// Datatables::of() mocks
+		// ******************************
+		$builder->shouldReceive('getConnection')->andReturn(m::mock('Illuminate\Database\Connection'));
+
+		// ******************************
+		// Datatables::make() mocks
+		// ******************************
+		$builder->shouldReceive('toSql')->times(4)->andReturn('select id, name from users');
+		$builder->getConnection()->shouldReceive('raw')->once()->andReturn('select \'1\' as row_count');
+		$builder->shouldReceive('select')->once()->andReturn($builder);
+		$builder->getConnection()->shouldReceive('raw')->andReturn('(select id, name from users) count_row_table');
+		$builder->shouldReceive('select')->once()->andReturn($builder);
+		$builder->getConnection()->shouldReceive('table')->times(2)->andReturn($builder);
+		$builder->shouldReceive('getBindings')->times(2)->andReturn(array());
+		$builder->shouldReceive('setBindings')->times(2)->with(array())->andReturn($builder);
+		$builder->shouldReceive('skip')->once()->andReturn($builder);
+		$builder->shouldReceive('take')->once()->andReturn($builder);
+		$builder->shouldReceive('count')->times(2)->andReturn(2);
+
+		Config::shouldReceive('get');
+
+		// set Input variables
+		$this->setNewVersionInputVariables();
+
+		$response = Datatables::of($builder)->make();
+		$actual = $response->getContent();
+		$expected = '{"draw":1,"recordsTotal":2,"recordsFiltered":2,"data":[[1,"foo"],[2,"bar"]]}';
 
 		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
 		$this->assertEquals($expected, $actual);
@@ -111,6 +161,7 @@ class DatatablesBuilderTest extends PHPUnit_Framework_TestCase  {
 		Config::shouldReceive('get');
 
 		// set Input variables
+		$_GET = [];
 		$_GET['sEcho'] = 1;
 
 		$builder->shouldReceive('where')->once()->andReturn($builder);
@@ -164,6 +215,7 @@ class DatatablesBuilderTest extends PHPUnit_Framework_TestCase  {
 		Config::shouldReceive('get');
 
 		// set Input variables
+		$_GET = [];
 		$_GET['sEcho'] = 1;
 
 		$response = Datatables::of($builder)->make(true);
@@ -172,6 +224,73 @@ class DatatablesBuilderTest extends PHPUnit_Framework_TestCase  {
 
 		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
 		$this->assertEquals($expected, $actual);
+	}
+
+	public function test_datatables_make_with_data_and_uses_mdata_1_10()
+	{
+		$cache = m::mock('stdClass');
+		$driver = m::mock('stdClass');
+		$data = array(
+			array('id' => 1, 'name' => 'foo'),
+			array('id' => 2, 'name' => 'bar'),
+			);
+		$builder = m::mock('Illuminate\Database\Query\Builder');
+		$builder->shouldReceive('select')->once()->with(array('id','name'))->andReturn($builder);
+		$builder->shouldReceive('from')->once()->with('users')->andReturn($builder);
+		$builder->shouldReceive('get')->once()->andReturn($data);
+
+		$builder->columns = array('id', 'name');
+		$builder->select(array('id', 'name'))->from('users');
+
+		// ******************************
+		// Datatables::of() mocks
+		// ******************************
+		$builder->shouldReceive('getConnection')->andReturn(m::mock('Illuminate\Database\Connection'));
+
+		// ******************************
+		// Datatables::make() mocks
+		// ******************************
+		$builder->shouldReceive('toSql')->times(4)->andReturn('select id, name from users');
+		$builder->getConnection()->shouldReceive('raw')->once()->andReturn('select \'1\' as row_count');
+		$builder->shouldReceive('select')->once()->andReturn($builder);
+		$builder->getConnection()->shouldReceive('raw')->andReturn('(select id, name from users) count_row_table');
+		$builder->shouldReceive('select')->once()->andReturn($builder);
+		$builder->getConnection()->shouldReceive('table')->times(2)->andReturn($builder);
+		$builder->shouldReceive('getBindings')->times(2)->andReturn(array());
+		$builder->shouldReceive('setBindings')->times(2)->with(array())->andReturn($builder);
+		$builder->shouldReceive('skip')->once()->andReturn($builder);
+		$builder->shouldReceive('take')->once()->andReturn($builder);
+		$builder->shouldReceive('count')->times(2)->andReturn(2);
+
+		Config::shouldReceive('get');
+
+		// set Input variables
+		$this->setNewVersionInputVariables();
+
+		$response = Datatables::of($builder)->make(true);
+		$actual = $response->getContent();
+		$expected = '{"draw":1,"recordsTotal":2,"recordsFiltered":2,"data":[{"id":1,"name":"foo"},{"id":2,"name":"bar"}]}';
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertEquals($expected, $actual);
+	}
+
+	protected function setNewVersionInputVariables()
+	{
+		$_GET = [];
+		$_GET['draw'] = 1;
+		$_GET['start'] = 1;
+		$_GET['length'] = 10;
+		$_GET['search']['value'] = '';
+		$_GET['columns'][0]['name'] = 'foo';
+		$_GET['columns'][0]['search']['value'] = '';
+		$_GET['columns'][0]['searchable'] = true;
+		$_GET['columns'][0]['orderable'] = true;
+		$_GET['columns'][1]['name'] = 'bar';
+		$_GET['columns'][1]['search']['value'] = '';
+		$_GET['columns'][1]['searchable'] = true;
+		$_GET['columns'][1]['orderable'] = true;
+
 	}
 
 	protected function getBuilder()
