27518b22722836dc7c4e57ac717796966d255132
Arjay Angeles <aqangeles@gmail.com>
2015-05-21 12:30:19 +0800

    added api docs

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","yajra\\Datatables\\Datatables"],["c","yajra\\Datatables\\DatatablesServiceProvider"],["c","yajra\\Datatables\\Engine\\BaseEngine"],["c","yajra\\Datatables\\Engine\\CollectionEngine"],["c","yajra\\Datatables\\Engine\\EloquentEngine"],["c","yajra\\Datatables\\Engine\\EngineContract"],["c","yajra\\Datatables\\Engine\\QueryBuilderEngine"],["c","yajra\\Datatables\\Facades\\Profiler"]];
