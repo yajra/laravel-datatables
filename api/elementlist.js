28afae3b1bd26a1e42077237a83685cacebb687c
Arjay Angeles <aqangeles@gmail.com>
2015-06-19 17:20:09 +0800

    update api docs

--- api/elementlist.js
+++ api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","yajra\\Datatables\\Datatables"],["c","yajra\\Datatables\\DatatablesServiceProvider"],["c","yajra\\Datatables\\Engine\\BaseEngine"],["c","yajra\\Datatables\\Engine\\CollectionEngine"],["c","yajra\\Datatables\\Engine\\EloquentEngine"],["c","yajra\\Datatables\\Engine\\EngineContract"],["c","yajra\\Datatables\\Engine\\QueryBuilderEngine"],["c","yajra\\Datatables\\Facades\\Profiler"]];
+ApiGen.elements = [["c","yajra\\Datatables\\Datatables"],["c","yajra\\Datatables\\DatatablesServiceProvider"],["c","yajra\\Datatables\\Engine\\BaseEngine"],["c","yajra\\Datatables\\Engine\\CollectionEngine"],["c","yajra\\Datatables\\Engine\\EloquentEngine"],["c","yajra\\Datatables\\Engine\\EngineContract"],["c","yajra\\Datatables\\Engine\\QueryBuilderEngine"],["c","yajra\\Datatables\\Facades\\Datatables"],["c","yajra\\Datatables\\Html\\Builder"],["c","yajra\\Datatables\\Html\\Column"],["c","yajra\\Datatables\\Html\\Parameters"]];
