f9320abf617e470e58e0601ecd4c8266f61c9086
Arjay Angeles <aqangeles@gmail.com>
2015-12-02 21:56:48 +0800

    Automatically register 3rd party facades dependency..

--- src/DatatablesServiceProvider.php
+++ src/DatatablesServiceProvider.php
@@ -32,35 +32,6 @@ class DatatablesServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->register(HtmlServiceProvider::class);
-        $this->app->register(ExcelServiceProvider::class);
-
-        $this->app['datatables'] = $this->app->share(
-            function ($app) {
-                $request = $app->make(Request::class);
-
-                return new Datatables($request);
-            }
-        );
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return string[]
-     */
-    public function provides()
-    {
-        return ['datatables'];
-    }
-
-    /**
      * Publish datatables assets.
      */
     private function publishAssets()
@@ -85,5 +56,56 @@ class DatatablesServiceProvider extends ServiceProvider
     {
         $this->commands(DataTablesMakeCommand::class);
         $this->commands(DataTablesScopeCommand::class);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerRequiredProviders();
+
+        $this->registerAliases();
+
+        $this->app['datatables'] = $this->app->share(
+            function ($app) {
+                $request = $app->make(Request::class);
+
+                return new Datatables($request);
+            }
+        );
+    }
+
+    /**
+     * Register 3rd party providers.
+     */
+    private function registerRequiredProviders()
+    {
+        $this->app->register(HtmlServiceProvider::class);
+        $this->app->register(ExcelServiceProvider::class);
+    }
+
+    /**
+     * Create aliases for the dependency.
+     */
+    private function registerAliases()
+    {
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Datatables', \Yajra\Datatables\Facades\Datatables::class);
+        $loader->alias('Form', \Collective\Html\FormFacade::class);
+        $loader->alias('HTML', \Collective\Html\HtmlFacade::class);
+        $loader->alias('Excel', \Maatwebsite\Excel\Facades\Excel::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return string[]
+     */
+    public function provides()
+    {
+        return ['datatables'];
     }
 }
