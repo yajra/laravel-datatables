212fbcd34aa26d29a40b33d8261011537649be7c
Arjay Angeles <aqangeles@gmail.com>
2016-04-29 12:47:35 +0800

    Use &nbsp; as default footer content.

--- src/Html/Column.php
+++ src/Html/Column.php
@@ -22,7 +22,7 @@ class Column extends Fluent
         $attributes['searchable'] = isset($attributes['searchable']) ? $attributes['searchable'] : true;
         $attributes['exportable'] = isset($attributes['exportable']) ? $attributes['exportable'] : true;
         $attributes['printable']  = isset($attributes['printable']) ? $attributes['printable'] : true;
-        $attributes['footer']     = isset($attributes['footer']) ? $attributes['footer'] : '';
+        $attributes['footer']     = isset($attributes['footer']) ? $attributes['footer'] : '&nbsp;';
 
         // Allow methods override attribute value
         foreach ($attributes as $attribute => $value) {
