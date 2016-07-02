9cd36c1fae621a2f1c54a2179d42f2240c7b1e4d
Arjay Angeles <aqangeles@gmail.com>
2016-07-02 13:46:38 +0800

    Add table stripes style.

--- src/resources/views/print.blade.php
+++ src/resources/views/print.blade.php
@@ -13,7 +13,7 @@
         </style>
     </head>
     <body>
-        <table class="table table-bordered table-condensed">
+        <table class="table table-bordered table-condensed table-striped">
             @foreach($data as $row)
                 @if ($row == reset($data)) 
                     <tr>
