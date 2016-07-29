61689ba7351f7a662689c772ddb4e738808b1d5d
alex82 <eagledata@gmail.com>
2016-07-29 13:56:11 +0200

    switched export and print buttons to use post request

--- src/resources/views/script.blade.php
+++ src/resources/views/script.blade.php
@@ -1,1 +1,6 @@
+// add csrf token, need for post requests
+$.extend(true, $.fn.dataTable.defaults, {
+    csrf_token : '{{ csrf_token() }}'
+});
+
 (function(window,$){window.LaravelDataTables=window.LaravelDataTables||{};window.LaravelDataTables["%1$s"]=$("#%1$s").DataTable(%2$s);})(window,jQuery);
