c02cb0867d87c2964f446ce7649102f593cd30d0
mehdi <mehdi.atraimche@vpscorp.ma>
2016-11-22 10:51:43 +0000

    add override auto filter and add ordering by nulls last

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,11 @@ return [
     'search' => [
         'case_insensitive' => true,
         'use_wildcards'    => false,
+    ],
+
+    'order' => [
+        //first %s column, second %s order direction
+        'nulls_last_sql' => '%s %s NULLS LAST',
     ]
 
 ];
