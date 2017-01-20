22727599996ce1418eee71054141422dc445377d
Arjay Angeles <aqangeles@gmail.com>
2017-01-20 14:13:05 +0800

    Add engines tests.

--- tests/DatatablesTest.php
+++ tests/DatatablesTest.php
@@ -1,14 +1,23 @@
 <?php
 
-
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Query\Grammars\Grammar;
+use Illuminate\Database\Query\Processors\Processor;
+use Mockery as m;
 use Yajra\Datatables\Datatables;
+use Yajra\Datatables\Engines\CollectionEngine;
+use Yajra\Datatables\Engines\EloquentEngine;
+use Yajra\Datatables\Engines\QueryBuilderEngine;
 use Yajra\Datatables\Request;
 
 class DatatablesTest extends PHPUnit_Framework_TestCase
 {
-    public function test_get_request()
+    /** @test */
+    public function it_will_return_the_request_instance()
     {
-        $datatables = $this->getDatatables();
+        $datatables = $this->createDataTable();
         $request    = $datatables->getRequest();
 
         $this->assertInstanceOf('Yajra\Datatables\Request', $request);
@@ -17,10 +26,49 @@ class DatatablesTest extends PHPUnit_Framework_TestCase
     /**
      * @return \Yajra\Datatables\Datatables
      */
-    protected function getDatatables()
+    protected function createDataTable()
     {
         $datatables = new Datatables(Request::capture());
 
         return $datatables;
+    }
+
+    /** @test */
+    public function it_will_return_query_builder_engine()
+    {
+        $dataTable  = $this->createDataTable();
+        $connection = $this->createMock(Connection::class);
+        $engine     = $dataTable->queryBuilder(new QueryBuilder($connection));
+        $this->assertInstanceOf(QueryBuilderEngine::class, $engine);
+    }
+
+    /** @test */
+    public function it_will_return_eloquent_engine()
+    {
+        $dataTable = $this->createDataTable();
+        $engine    = $dataTable->eloquent(UserModelStub::query());
+        $this->assertInstanceOf(EloquentEngine::class, $engine);
+    }
+
+    /** @test */
+    public function it_will_return_collection_engine()
+    {
+        $dataTable = $this->createDataTable();
+        $engine    = $dataTable->collection(collect([]));
+        $this->assertInstanceOf(CollectionEngine::class, $engine);
+    }
+}
+
+class UserModelStub extends Model
+{
+    public function getConnection()
+    {
+        $connection = m::mock(Connection::class);
+        $connection->shouldReceive('getQueryGrammar')->andReturn(new Grammar);
+        $connection->shouldReceive('getPostProcessor')->andReturn(new Processor);
+        $connection->shouldReceive('getTablePrefix')->andReturn('');
+        $connection->shouldReceive('getDriverName')->andReturn('');
+
+        return $connection;
     }
 }
