49587f4dbd71eff5bedfac78c26ba43a922d0e4f
Arjay Angeles <aqangeles@gmail.com>
2017-06-29 15:20:35 +0800

    Fix fetching of column defs.

--- src/Engines/BaseEngine.php
+++ src/Engines/BaseEngine.php
@@ -601,7 +601,7 @@ abstract class BaseEngine implements DataTableEngineContract
         $config  = config('datatables.columns');
         $allowed = ['excess', 'escape', 'raw', 'blacklist', 'whitelist'];
 
-        return array_merge_recursive($this->columnDef, array_only($config, $allowed));
+        return array_merge(array_only($config, $allowed), $this->columnDef);
     }
 
     /**
