f28458a5032faeeb344697bdd25c97b6767d56b3
Arjay Angeles <aqangeles@gmail.com>
2017-08-01 11:54:01 +0800

    Fix merging of default config.

--- src/Engines/BaseEngine.php
+++ src/Engines/BaseEngine.php
@@ -247,7 +247,7 @@ abstract class BaseEngine implements DataTableEngineContract
     public function removeColumn()
     {
         $names                     = func_get_args();
-        $this->columnDef['excess'] = array_merge($this->columnDef['excess'], $names);
+        $this->columnDef['excess'] = array_merge($this->getColumnsDefinition()['excess'], $names);
 
         return $this;
     }
@@ -925,7 +925,7 @@ abstract class BaseEngine implements DataTableEngineContract
     public function pushToBlacklist($column)
     {
         if (! $this->isBlacklisted($column)) {
-            array_push($this->columnDef['blacklist'], $column);
+            $this->columnDef['blacklist'][] = $column;
         }
 
         return $this;
@@ -939,11 +939,13 @@ abstract class BaseEngine implements DataTableEngineContract
      */
     protected function isBlacklisted($column)
     {
-        if (in_array($column, $this->columnDef['blacklist'])) {
+        $colDef = $this->getColumnsDefinition();
+
+        if (in_array($column, $colDef['blacklist'])) {
             return true;
         }
 
-        if ($this->columnDef['whitelist'] === '*' || in_array($column, $this->columnDef['whitelist'])) {
+        if ($colDef['whitelist'] === '*' || in_array($column, $colDef['whitelist'])) {
             return false;
         }
 
