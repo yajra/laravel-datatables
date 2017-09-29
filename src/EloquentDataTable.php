4b27791caab6f606fd407b8c8bdf4089b5f7639d
Elf Sundae <elf.sundae@gmail.com>
2017-09-29 21:41:03 +0800

    Add addColumns() to EloquentDataTable

--- src/EloquentDataTable.php
+++ src/EloquentDataTable.php
@@ -29,6 +29,28 @@ class EloquentDataTable extends QueryDataTable
     }
 
     /**
+     * Add columns in collection.
+     *
+     * @param  array  $names
+     * @param  bool|int  $order
+     * @return $this
+     */
+    public function addColumns(array $names, $order = false)
+    {
+        foreach ($names as $name => $attribute) {
+            if (is_int($name)) {
+                $name = $attribute;
+            }
+
+            $this->addColumn($name, function ($model) use ($attribute) {
+                return $model->getAttribute($attribute);
+            }, is_int($order) ? $order++ : $order);
+        }
+
+        return $this;
+    }
+
+    /**
      * If column name could not be resolved then use primary key.
      *
      * @return string
