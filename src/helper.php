d9610260870d3ac4ac1f409c5ac4d694ba264e30
Elf Sundae <elf.sundae@gmail.com>
2017-10-28 15:38:37 +0800

    Fix datatables() helper

--- src/helper.php
+++ src/helper.php
@@ -1,21 +1,19 @@
 <?php
 
-use Yajra\DataTables\DataTables;
-
 if (! function_exists('datatables')) {
     /**
      * Helper to make a new DataTable instance from source.
-     * Or return a new factory if source is not set.
+     * Or return the factory if source is not set.
      *
      * @param mixed $source
      * @return \Yajra\DataTables\DataTableAbstract|\Yajra\DataTables\DataTables
      */
     function datatables($source = null)
     {
-        if ($source) {
-            return DataTables::make($source);
+        if (is_null($source)) {
+            return app('datatables');
         }
 
-        return new DataTables;
+        return call_user_func_array([app('datatables'), 'make'], func_get_args());
     }
 }
