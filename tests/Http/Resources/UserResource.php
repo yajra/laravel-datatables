3cdd6ecc07a15ea06adbdbd58c16f2d77eb925ca
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-04-11 15:39:43 +0530

    Fix Failed test on Laravel 5.5

--- tests/Http/Resources/UserResource.php
+++ tests/Http/Resources/UserResource.php
@@ -2,9 +2,9 @@
 
 namespace Yajra\DataTables\Tests\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\Resource;
 
-class UserResource extends JsonResource
+class UserResource extends Resource
 {
     /**
      * Transform the resource into an array.
