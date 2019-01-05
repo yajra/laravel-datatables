2a034d646ce622d6e7f9701c0430f3e293223cb5
Arjay Angeles <aqangeles@gmail.com>
2019-01-05 10:02:53 +0800

    Revert "[8.0] Keep casted attributes #1747"

--- src/Processors/DataProcessor.php
+++ src/Processors/DataProcessor.php
@@ -232,11 +232,7 @@ class DataProcessor
             } elseif (is_array($this->escapeColumns)) {
                 $columns = array_diff($this->escapeColumns, $this->rawColumns);
                 foreach ($columns as $key) {
-                    $data = array_get($row, $key);
-                    $dataType = gettype($data);
-                    $data = e($data);
-                    settype($data, $dataType);
-                    array_set($row, $key, $data);
+                    array_set($row, $key, e(array_get($row, $key)));
                 }
             }
 
@@ -255,9 +251,7 @@ class DataProcessor
         $arrayDot = array_filter(array_dot($row));
         foreach ($arrayDot as $key => $value) {
             if (! in_array($key, $this->rawColumns)) {
-                $dataType       = gettype($value);
                 $arrayDot[$key] = e($value);
-                settype($arrayDot[$key], $dataType);
             }
         }
 
--- src/Utilities/Helper.php
+++ src/Utilities/Helper.php
@@ -155,9 +155,7 @@ class Helper
      */
     public static function convertToArray($row)
     {
-        $data = $row instanceof Arrayable ?
-                $row->toArray() :
-                ($row instanceof \StdClass ? (array) $row : $row);
+        $data = $row instanceof Arrayable ? $row->toArray() : (array) $row;
 
         foreach ($data as &$value) {
             if (is_object($value) || is_array($value)) {
