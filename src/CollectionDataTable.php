3370cd69a31a0c409c4bd49485bb68a666c79cbd
Armin Preiml <apreiml@strohwolke.at>
2019-05-07 09:21:33 +0200

    consider blacklist in collection column search

--- src/CollectionDataTable.php
+++ src/CollectionDataTable.php
@@ -93,35 +93,38 @@ class CollectionDataTable extends DataTableAbstract
     {
         $columns = $this->request->get('columns', []);
         for ($i = 0, $c = count($columns); $i < $c; $i++) {
-            if ($this->request->isColumnSearchable($i)) {
-                $this->isFilterApplied = true;
+            $column  = $this->getColumnName($i);
 
-                $regex   = $this->request->isRegex($i);
-                $column  = $this->getColumnName($i);
-                $keyword = $this->request->columnKeyword($i);
-
-                $this->collection = $this->collection->filter(
-                    function ($row) use ($column, $keyword, $regex) {
-                        $data = $this->serialize($row);
-
-                        $value = Arr::get($data, $column);
-
-                        if ($this->config->isCaseInsensitive()) {
-                            if ($regex) {
-                                return preg_match('/' . $keyword . '/i', $value) == 1;
-                            }
-
-                            return strpos(Str::lower($value), Str::lower($keyword)) !== false;
-                        }
-
-                        if ($regex) {
-                            return preg_match('/' . $keyword . '/', $value) == 1;
-                        }
-
-                        return strpos($value, $keyword) !== false;
-                    }
-                );
+            if (! $this->request->isColumnSearchable($i) || $this->isBlacklisted($column)) {
+                continue;
             }
+
+            $this->isFilterApplied = true;
+
+            $regex   = $this->request->isRegex($i);
+            $keyword = $this->request->columnKeyword($i);
+
+            $this->collection = $this->collection->filter(
+                function ($row) use ($column, $keyword, $regex) {
+                    $data = $this->serialize($row);
+
+                    $value = Arr::get($data, $column);
+
+                    if ($this->config->isCaseInsensitive()) {
+                        if ($regex) {
+                            return preg_match('/' . $keyword . '/i', $value) == 1;
+                        }
+
+                        return strpos(Str::lower($value), Str::lower($keyword)) !== false;
+                    }
+
+                    if ($regex) {
+                        return preg_match('/' . $keyword . '/', $value) == 1;
+                    }
+
+                    return strpos($value, $keyword) !== false;
+                }
+            );
         }
     }
 
