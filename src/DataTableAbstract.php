7728c62281baea939ba699ef6158b439e1439649
Mike P. Sinn <m@quantimodo.com>
2020-04-30 16:13:20 -0500

    StyleCI Mandates

--- src/DataTableAbstract.php
+++ src/DataTableAbstract.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Macroable;
 use Psr\Log\LoggerInterface;
 use Yajra\DataTables\Contracts\DataTable;
-use Yajra\DataTables\Exceptions\Exception;
 use Yajra\DataTables\Processors\DataProcessor;
 use Yajra\DataTables\Utilities\Helper;
 
