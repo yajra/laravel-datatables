a9cda1d5289fa9256e30572a5bab37243c2ef261
hpacleb <kimpacleb@gmail.com>
2022-10-07 15:41:39 +0800

    Add ApiResourceDataTable

--- src/config/datatables.php
+++ src/config/datatables.php
@@ -48,6 +48,7 @@ return [
         'eloquent'   => Yajra\DataTables\EloquentDataTable::class,
         'query'      => Yajra\DataTables\QueryDataTable::class,
         'collection' => Yajra\DataTables\CollectionDataTable::class,
+        'resource' => Yajra\DataTables\ApiResourceDataTable::class,
     ],
 
     /*
