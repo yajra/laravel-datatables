9f7169a1cea51ff22cf4154c5c0e6da35d19c558
Jurian Arie <28654085+JurianArie@users.noreply.github.com>
2024-07-09 09:37:13 +0200

    Make query for filteredRecords when totalRecords was manually set

--- src/QueryDataTable.php
+++ src/QueryDataTable.php
@@ -26,6 +26,11 @@ class QueryDataTable extends DataTableAbstract
     protected bool $prepared = false;
 
     /**
+     * Flag to check if the total records count query has been performed.
+     */
+    protected bool $performedTotalRecordsCount = false;
+
+    /**
      * Query callback for custom pagination using limit without offset.
      *
      * @var callable|null
@@ -158,6 +163,20 @@ class QueryDataTable extends DataTableAbstract
     }
 
     /**
+     * Count total items.
+     */
+    public function totalCount(): int
+    {
+        if ($this->totalRecords !== null) {
+            return $this->totalRecords;
+        }
+
+        $this->performedTotalRecordsCount = true;
+
+        return $this->totalRecords = $this->count();
+    }
+
+    /**
      * Counts current query.
      */
     public function count(): int
@@ -253,7 +272,7 @@ class QueryDataTable extends DataTableAbstract
 
         // If no modification between the original query and the filtered one has been made
         // the filteredRecords equals the totalRecords
-        if ($this->query == $initialQuery) {
+        if ($this->query == $initialQuery && $this->performedTotalRecordsCount) {
             $this->filteredRecords ??= $this->totalRecords;
         } else {
             $this->filteredCount();
